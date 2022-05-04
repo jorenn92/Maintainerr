1da6474555dc73cf9c221df33da60100a5fac438
jorenn92 <Jorenn92@gmail.com>
2022-05-04 17:17:04 +0200

    fix(migration): remove logic to move db from old location

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env ash
 
-# Temporary migrate db file to new location
-if [ -f "/opt/server/data/maintainerr.sqlite" ]; then
-    mv /opt/server/data/maintainerr.sqlite /opt/data/
-fi
-
-
 cd /opt/
 npx typeorm migration:run
 npm run docs-serve &
