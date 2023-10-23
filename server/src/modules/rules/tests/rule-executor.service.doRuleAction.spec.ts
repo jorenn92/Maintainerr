3b1432ae578f68c747c4bdb711d46c8cc3f31b3b
jorenn92 <Jorenn92@gmail.com>
2023-10-23 10:11:41 +0200

    test(rules): Add extra tests for undefined values

--- server/src/modules/rules/tests/rule-executor.service.doRuleAction.spec.ts
+++ server/src/modules/rules/tests/rule-executor.service.doRuleAction.spec.ts
@@ -39,6 +39,23 @@ describe('RuleExecutorService', () => {
       expect(result).toBe(true);
     });
 
+    it('should return false when comparing strings with action EQUALS and value is an empty string', () => {
+      const val1 = 'abc';
+      const val2 = '';
+      const action = RulePossibility.EQUALS;
+      const result = ruleExecutorService['doRuleAction'](val1, val2, action);
+      expect(result).toBe(false);
+    });
+
+
+    it('should return false when comparing two strings with action EQUALS and value is undefined', () => {
+      const val1 = 'abc';
+      const val2 = undefined;
+      const action = RulePossibility.EQUALS;
+      const result = ruleExecutorService['doRuleAction'](val1, val2, action);
+      expect(result).toBe(false);
+    });
+
     it('should return false when comparing two strings with action EQUALS', () => {
       const val1 = 'abc';
       const val2 = 'abd';
@@ -167,6 +184,30 @@ describe('RuleExecutorService', () => {
       expect(result).toBe(true);
     });
 
+    it('should return false when comparing an array of strings with action CONTAINS and value is an empty string', () => {
+      const val1 = ['abc', 'def'];
+      const val2 = [''];
+      const action = RulePossibility.CONTAINS;
+      const result = ruleExecutorService['doRuleAction'](val1, val2, action);
+      expect(result).toBe(false);
+    });
+
+    it('should return false when comparing an array of strings with action CONTAINS and value contains undefined', () => {
+      const val1 = ['abc', 'def'];
+      const val2 = ['ral', undefined, 'rel'];
+      const action = RulePossibility.CONTAINS;
+      const result = ruleExecutorService['doRuleAction'](val1, val2, action);
+      expect(result).toBe(false);
+    });
+
+    it('should return true when comparing an array of strings with action CONTAINS and value contains undefined', () => {
+      const val1 = ['abc', 'def'];
+      const val2 = ['ral', undefined, 'abc'];
+      const action = RulePossibility.CONTAINS;
+      const result = ruleExecutorService['doRuleAction'](val1, val2, action);
+      expect(result).toBe(true);
+    });
+
     it('should return false when comparing an array of strings with action CONTAINS', () => {
       const val1 = ['abc', 'def'];
       const val2 = ['ghi'];
@@ -215,6 +256,14 @@ describe('RuleExecutorService', () => {
       expect(result).toBe(true);
     });
 
+    it('should return true when comparing an array of numbers against 1 value with action NOT_CONTAINS and value contains undefined', () => {
+      const val1 = [1, 2, 3, 4];
+      const val2 = [5, undefined, 6];
+      const action = RulePossibility.NOT_CONTAINS;
+      const result = ruleExecutorService['doRuleAction'](val1, val2, action);
+      expect(result).toBe(true);
+    });
+
     it('should return true when comparing an array of numbers against 1 value with action CONTAINS', () => {
       const val1 = [1, 2, 3, 4];
       const val2 = [3];
@@ -233,7 +282,7 @@ describe('RuleExecutorService', () => {
 
     it('should return false when comparing 2 arrays of multiple numbers with action CONTAINS', () => {
       const val1 = [1, 2, 3, 4];
-      const val2 = [3, 5];
+      const val2 = [6, 5];
       const action = RulePossibility.CONTAINS;
       const result = ruleExecutorService['doRuleAction'](val1, val2, action);
       expect(result).toBe(false);
@@ -247,12 +296,12 @@ describe('RuleExecutorService', () => {
       expect(result).toBe(true);
     });
 
-    it('should return true when comparing an array of multiple numbers with action NOT_CONTAINS', () => {
+    it('should return false when comparing an array of multiple numbers with action NOT_CONTAINS', () => {
       const val1 = [1, 2, 3, 4];
       const val2 = [3, 5];
       const action = RulePossibility.NOT_CONTAINS;
       const result = ruleExecutorService['doRuleAction'](val1, val2, action);
-      expect(result).toBe(true);
+      expect(result).toBe(false);
     });
 
     it('should return true when comparing two numbers with action BIGGER', () => {
@@ -266,6 +315,14 @@ describe('RuleExecutorService', () => {
     it('should return false when comparing two numbers with action SMALLER', () => {
       const val1 = 5;
       const val2 = 3;
+      const action = RulePossibility.SMALLER;
+      const result = ruleExecutorService['doRuleAction'](val1, val2, action);
+      expect(result).toBe(false);
+    });
+
+    it('should return false when comparing two numbers with action SMALLER and value is undefined', () => {
+      const val1 = 5;
+      const val2 = undefined;
       const action = RulePossibility.SMALLER;
       const result = ruleExecutorService['doRuleAction'](val1, val2, action);
       expect(result).toBe(false);
@@ -295,6 +352,14 @@ describe('RuleExecutorService', () => {
       expect(result).toBe(true);
     });
 
+    it('should return false when comparing two dates with action BEFORE and value is undefined', () => {
+      const val1 = new Date('2022-01-01');
+      const val2 = undefined;
+      const action = RulePossibility.BEFORE;
+      const result = ruleExecutorService['doRuleAction'](val1, val2, action);
+      expect(result).toBe(false);
+    });
+
     it('should return false when comparing two dates with action BEFORE', () => {
       const val1 = new Date('2022-01-03');
       const val2 = new Date('2022-01-02');
@@ -319,6 +384,14 @@ describe('RuleExecutorService', () => {
       expect(result).toBe(false);
     });
 
+    it('should return false when comparing two dates with action AFTER and value is undefined', () => {
+      const val1 = new Date('2022-01-01');
+      const val2 = undefined;
+      const action = RulePossibility.AFTER;
+      const result = ruleExecutorService['doRuleAction'](val1, val2, action);
+      expect(result).toBe(false);
+    });
+
     it('should return true when comparing a date with action IN_LAST', () => {
       const val1 = new Date(Date.now() - 1000); // One second ago
       const val2 = new Date(new Date().getTime() - +3600 * 1000); // 1 hour in seconds
@@ -335,6 +408,14 @@ describe('RuleExecutorService', () => {
       expect(result).toBe(false);
     });
 
+    it('should return false when comparing a date with action IN_LAST and value is undefined', () => {
+      const val1 = new Date(Date.now() - 3600 * 2000); // More than 1 hour ago
+      const val2 = undefined;
+      const action = RulePossibility.IN_LAST;
+      const result = ruleExecutorService['doRuleAction'](val1, val2, action);
+      expect(result).toBe(false);
+    });
+
     it('should return true when comparing a date with action IN_NEXT', () => {
       const val1 = new Date(new Date().getTime() + +432000 * 1000); // 5 days from now
       const val2 = new Date(new Date().getTime() + +864000 * 1000); // 10 days from now
@@ -346,6 +427,14 @@ describe('RuleExecutorService', () => {
     it('should return false when comparing a date with action IN_NEXT', () => {
       const val1 = new Date(new Date().getTime() + +865000 * 1000); // More than 10 days from now
       const val2 = new Date(new Date().getTime() + +864000 * 1000); // 10 days from now
+      const action = RulePossibility.IN_NEXT;
+      const result = ruleExecutorService['doRuleAction'](val1, val2, action);
+      expect(result).toBe(false);
+    });
+
+    it('should return false when comparing a date with action IN_NEXT and value is undefined', () => {
+      const val1 = new Date(new Date().getTime() + +865000 * 1000); // More than 10 days from now
+      const val2 = undefined; // 10 days from now
       const action = RulePossibility.IN_NEXT;
       const result = ruleExecutorService['doRuleAction'](val1, val2, action);
       expect(result).toBe(false);
