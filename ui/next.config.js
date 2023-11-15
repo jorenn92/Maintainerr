07047ebb0b4cbe819e14ee473bda47f4d5e9d9c7
Jorenn92 <jorenn92@gmail.com>
2023-11-15 16:42:04 +0100

    refactor(ui): migrate next/image config to remotePattern

--- ui/next.config.js
+++ ui/next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['image.tmdb.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   async rewrites() {
     return [
