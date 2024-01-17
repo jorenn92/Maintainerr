9c10e44d0662eb8f700c962ad3d7e5be8bfc9de4
jorenn92 <Jorenn92@gmail.com>
2024-01-17 13:15:32 +0100

    chore: Center the test media button

--- ui/src/components/Collection/CollectionDetail/index.tsx
+++ ui/src/components/Collection/CollectionDetail/index.tsx
@@ -165,14 +165,15 @@ const CollectionDetail: React.FC<ICollectionDetail> = (
             />
           </div>
         </div>
-
-        <button
-          className="edit-button mb-4 flex  rounded h-9 text-zinc-200 shadow-md"
-          onClick={() => setMediaTestModalOpen(true)}
-        >
-          {<PlayIcon className="m-auto h-5 ml-5" />}{' '}
-          <p className="m-auto rules-button-text ml-1 mr-5">Test Media</p>
-        </button>
+        <div className="flex justify-center sm:justify-start">
+          <button
+            className="edit-button mb-4 flex  rounded h-9 text-zinc-200 shadow-md"
+            onClick={() => setMediaTestModalOpen(true)}
+          >
+            {<PlayIcon className="m-auto h-5 ml-5" />}{' '}
+            <p className="m-auto rules-button-text ml-1 mr-5">Test Media</p>
+          </button>
+        </div>
 
         {selectedTab === 'media' ? (
           <OverviewContent
