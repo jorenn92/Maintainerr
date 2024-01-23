bc479dd6b20162f3caf38aba1267efe45d1b786e
jorenn92 <Jorenn92@gmail.com>
2024-01-23 17:05:23 +0100

    refactor(database): Enhanced TypeORM Implementation, shifted running of migrations to the NestJS Process.

---
+++ server/src/app/config/typeOrmConfig.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const ormConfig: TypeOrmModuleOptions = {
+  type: 'sqlite',
+  logging: false,
+  database:
+    process.env.NODE_ENV === 'production'
+      ? '/opt/data/maintainerr.sqlite'
+      : '../data/maintainerr.sqlite',
+  subscribers: ['./**/*.subscriber{.ts,.js}'],
+  migrations:
+    process.env.NODE_ENV === 'production'
+      ? ['/opt/server/database/migrations/**/*{.js,.ts}']
+      : ['./dist/database/migrations/**/*{.js,.ts}'],
+  autoLoadEntities: true,
+  migrationsRun: true,
+};
+export = ormConfig;
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ash
-
-cd /opt/
-yarn migration:run
-
-/usr/bin/supervisord -c /etc/supervisord.conf
