89f8f0c73d8117438737c9beb46eaa8c51e7ab79
Joren V <9069536+jorenn92@users.noreply.github.com>
2024-08-14 16:27:07 +0200

    chore: Removed leftover Radarr addMovie() method (#1215)

--- server/src/modules/api/servarr-api/helpers/radarr.helper.ts
+++ server/src/modules/api/servarr-api/helpers/radarr.helper.ts
@@ -4,7 +4,6 @@ import {
   RadarrInfo,
   RadarrMovie,
   RadarrMovieFile,
-  RadarrMovieOptions,
 } from '../interfaces/radarr.interface';
 
 export class RadarrApi extends ServarrApi<{ movieId: number }> {
@@ -49,90 +48,6 @@ export class RadarrApi extends ServarrApi<{ movieId: number }> {
       this.logger.debug(e);
     }
   }
-
-  public addMovie = async (
-    options: RadarrMovieOptions,
-  ): Promise<RadarrMovie> => {
-    try {
-      const movie = await this.getMovieByTmdbId(options.tmdbId);
-
-      if (movie.downloaded) {
-        this.logger.log(
-          'Title already exists and is available. Skipping add and returning success',
-        );
-        return movie;
-      }
-
-      // movie exists in Radarr but is neither downloaded nor monitored
-      if (movie.id && !movie.monitored) {
-        const response = await this.axios.put<RadarrMovie>(`/movie`, {
-          ...movie,
-          title: options.title,
-          qualityProfileId: options.qualityProfileId,
-          profileId: options.profileId,
-          titleSlug: options.tmdbId.toString(),
-          minimumAvailability: options.minimumAvailability,
-          tmdbId: options.tmdbId,
-          year: options.year,
-          tags: options.tags,
-          rootFolderPath: options.rootFolderPath,
-          monitored: options.monitored,
-          addOptions: {
-            searchForMovie: options.searchNow,
-          },
-        });
-
-        if (response.data.monitored) {
-          this.logger.log(
-            'Found existing title in Radarr and set it to monitored.',
-          );
-
-          if (options.searchNow) {
-            this.searchMovie(response.data.id);
-          }
-
-          return response.data;
-        } else {
-          this.logger.warn('Failed to update existing movie in Radarr.');
-        }
-      }
-
-      if (movie.id) {
-        this.logger.log(
-          'Movie is already monitored in Radarr. Skipping add and returning success',
-        );
-        return movie;
-      }
-
-      const response = await this.axios.post<RadarrMovie>(`/movie`, {
-        title: options.title,
-        qualityProfileId: options.qualityProfileId,
-        profileId: options.profileId,
-        titleSlug: options.tmdbId.toString(),
-        minimumAvailability: options.minimumAvailability,
-        tmdbId: options.tmdbId,
-        year: options.year,
-        rootFolderPath: options.rootFolderPath,
-        monitored: options.monitored,
-        tags: options.tags,
-        addOptions: {
-          searchForMovie: options.searchNow,
-        },
-      });
-
-      if (response.data.id) {
-        this.logger.log('Radarr accepted request');
-      } else {
-        this.logger.warn('Failed to add movie to Radarr');
-      }
-      return response.data;
-    } catch (e) {
-      this.logger.warn(
-        'Failed to add movie to Radarr. This might happen if the movie already exists, in which case you can safely ignore this error.',
-      );
-      this.logger.debug(e);
-    }
-  };
 
   public async searchMovie(movieId: number): Promise<void> {
     this.logger.log('Executing movie search command');
