22c05b548208158f2a3357c42e145d7c27ccd3bc
ydkmlt84 <justin@tacohouse.us>
2024-10-25 12:51:01 -0400

    basic outline of ideas

--- ui/src/components/Settings/About/index.tsx
+++ ui/src/components/Settings/About/index.tsx
@@ -15,11 +15,12 @@ const AboutSettings = () => {
   useEffect(() => {
     document.title = 'Maintainerr - Settings - About'
   }, [])
-
+  // Timezone
   const getBrowserTimezone = (): string => {
     return Intl.DateTimeFormat().resolvedOptions().timeZone
   }
-
+  // End Timezone
+  // Maintainerr Version
   const [version, setVersion] = useState<string>('')
   const [commitTag, setCommitTag] = useState<string>('')
 
@@ -31,6 +32,7 @@ const AboutSettings = () => {
       }
     })
   }, [])
+  // End Maintainerr Version
 
   return (
     <div className="h-full w-full">
@@ -57,7 +59,7 @@ const AboutSettings = () => {
           </div>
         </div>
       </div>
-
+      {/* Maintainerr Portion */}
       <div className="section h-full w-full mb-2">
         <h3 className="heading">About Maintainerr</h3>
       </div>
@@ -101,19 +103,165 @@ const AboutSettings = () => {
             </div>
           </div>
         </div>
-      </div>
-      <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-      <div className="section h-full w-full">
-        <h3 className="heading">Radarr Statistics</h3>
-      </div>
-      <div className="section">
-        <div className="form-row">
-          <label className="text-label"> Version </label>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="form-row my-2">
+          <label htmlFor="name" className="text-label">
+            Number Of Rules
+          </label>
           <div className="form-input">
-            <div className="form-input-field"></div>
+            <div className="form-input-field">
+              <span className="">
+                <code>"Value goes here"</code>
+              </span>
+            </div>
+          </div>
+        </div>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="form-row my-2">
+          <label htmlFor="name" className="text-label">
+            Total Media in Collections
+          </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <span className="">
+                <code>"Value goes here"</code>
+              </span>
+            </div>
+          </div>
+        </div>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="form-row my-2">
+          <label htmlFor="name" className="text-label">
+            Space Saved
+          </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <span className="">
+                <code>"Value goes here"</code>
+              </span>
+            </div>
           </div>
         </div>
       </div>
+      <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      {/* End Maintainerr Portion */}
+      {/* Radarr Portion */}
+      <div className="section h-full w-full mb-2">
+        <h3 className="heading">Radarr Statistics</h3>
+      </div>
+      <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      <div className="section my-2">
+        <div className="form-row my-2">
+          <label className="text-label"> Version </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <code>"Value goes here"</code>
+            </div>
+          </div>
+        </div>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="form-row my-2">
+          <label className="text-label"> Status </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <code>"Value goes here"</code>
+            </div>
+          </div>
+        </div>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="form-row my-2">
+          <label className="text-label"> Diskspace Remaining </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <code>"Value goes here"</code>
+            </div>
+          </div>
+        </div>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="form-row my-2">
+          <label className="text-label"> Number of Movies </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <code>"Value goes here"</code>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      {/* End Radarr Portion */}
+      {/* Sonarr Portion */}
+      <div className="section h-full w-full mb-2">
+        <h3 className="heading">Sonarr Statistics</h3>
+      </div>
+      <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      <div className="section my-2">
+        <div className="form-row my-2">
+          <label className="text-label"> Version </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <code>"Value goes here"</code>
+            </div>
+          </div>
+        </div>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="form-row my-2">
+          <label className="text-label"> Status </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <code>"Value goes here"</code>
+            </div>
+          </div>
+        </div>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="form-row my-2">
+          <label className="text-label"> Diskspace Remaining </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <code>"Value goes here"</code>
+            </div>
+          </div>
+        </div>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="form-row my-2">
+          <label className="text-label">
+            {' '}
+            Number of Shows/Seasons/Episodes{' '}
+          </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <code>"Value goes here"</code>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      {/* End Sonarr Portion */}
+      {/* Overseerr Portion */}
+      <div className="section h-full w-full mb-2">
+        <h3 className="heading">Overseerr Statistics</h3>
+      </div>
+      <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      <div className="section my-2">
+        <div className="form-row my-2">
+          <label className="text-label"> Version </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <code>"Value goes here"</code>
+            </div>
+          </div>
+        </div>
+        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <div className="form-row my-2">
+          <label className="text-label"> Requests </label>
+          <div className="form-input">
+            <div className="form-input-field">
+              <code>"Value goes here"</code>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      {/* End Overseerr Portion */}
     </div>
   )
 }
