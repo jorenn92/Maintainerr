4c10978fc7951510f95e7291c0c5ca8cdbc21345
Nowadays <herespieceofmine@gmail.com>
2024-10-29 22:54:34 +0100

    chore: migrate to eslint flat package to support eslint V9

---
+++ server/eslint.config.mjs
@@ -0,0 +1,50 @@
+import typescriptEslintEslintPlugin from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/eslint.config.mjs"],
+}, ...compat.extends("plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"), {
+    plugins: {
+        "@typescript-eslint": typescriptEslintEslintPlugin,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.jest,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "module",
+
+        parserOptions: {
+            project: "tsconfig.json",
+            tsconfigRootDir: ".",
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+
+        "prettier/prettier": ["error", {
+            endOfLine: "auto",
+        }],
+    },
+}];
