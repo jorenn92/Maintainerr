b4189752ce265b16e2b28a7c4eecc0385903fe07
Ben Scobie <benscobie@users.noreply.github.com>
2024-11-08 09:16:26 +0000

    feat: Add envars to control service ports (#1333)

---
+++ ui/src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const apiPort = process.env.API_PORT || 3001
+
+export function middleware(request: NextRequest) {
+  const destination = new URL(`http://localhost:${apiPort}`)
+  const url = request.nextUrl.clone()
+  url.host = destination.host
+  url.port = destination.port
+  return NextResponse.rewrite(url)
+}
+
+export const config = {
+  matcher: '/api/:path*',
+}
