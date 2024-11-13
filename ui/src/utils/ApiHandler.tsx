bba6e729b1bbdc87c3a1a348a6ceb0817972985c
Ben Scobie <benscobie@users.noreply.github.com>
2024-11-13 23:18:24 +0000

    feat: Add base path support

--- ui/src/utils/ApiHandler.tsx
+++ ui/src/utils/ApiHandler.tsx
@@ -10,20 +10,24 @@ const ApiHandler = async <Response,>(
     payload?: any,
     method: 'get' | 'post' | 'delete' | 'put' = 'get',
   ) => {
+    const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? ''
+
     switch (method) {
       case 'get':
-        return await axios.get<Response>(`/api${url}`).then((res) => res.data)
+        return await axios
+          .get<Response>(`${basePath}/api${url}`)
+          .then((res) => res.data)
       case 'post':
         return await axios
-          .post<Response>(`/api${url}`, payload)
+          .post<Response>(`${basePath}/api${url}`, payload)
           .then((res) => res.data)
       case 'put':
         return await axios
-          .put<Response>(`/api${url}`, payload)
+          .put<Response>(`${basePath}/api${url}`, payload)
           .then((res) => res.data)
       case 'delete':
         return await axios
-          .delete<Response>(`/api${url}`, payload)
+          .delete<Response>(`${basePath}/api${url}`, payload)
           .then((res) => res.data)
     }
   }
