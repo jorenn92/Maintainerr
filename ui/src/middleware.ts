9597bfa69f29e508e8762ddfad1d2c3cccd39528
Ben Scobie <benscobie@users.noreply.github.com>
2024-11-18 09:57:25 +0000

    feat: Add base path support (#1373)

--- ui/src/middleware.ts
+++ ui/src/middleware.ts
@@ -4,14 +4,13 @@ import type { NextRequest } from 'next/server'
 const apiPort = process.env.API_PORT || 3001
 
 export function middleware(request: NextRequest) {
-  const destination = new URL(`http://localhost:${apiPort}`)
-  const url = request.nextUrl.clone()
-  url.host = destination.host
-  url.port = destination.port
-  url.protocol = destination.protocol
-  return NextResponse.rewrite(url)
-}
-
-export const config = {
-  matcher: '/api/:path*',
+  if (request.nextUrl.pathname.startsWith('/api/')) {
+    const url = request.nextUrl.clone()
+    const destination = new URL(`http://localhost:${apiPort}`)
+    url.host = destination.host
+    url.port = destination.port
+    url.protocol = destination.protocol
+    url.basePath = ''
+    return NextResponse.rewrite(url)
+  }
 }
