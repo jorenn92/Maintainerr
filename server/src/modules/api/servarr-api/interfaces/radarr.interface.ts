800bcdccaf81dafd1dae3b8b86ace27284b3b684
Ben Scobie <benscobie@users.noreply.github.com>
2024-11-28 23:06:24 +0000

    feat: Add "Original Language" rule to Sonarr & Radarr

--- server/src/modules/api/servarr-api/interfaces/radarr.interface.ts
+++ server/src/modules/api/servarr-api/interfaces/radarr.interface.ts
@@ -14,6 +14,7 @@ export interface RadarrMovieOptions {
 export interface RadarrMovie {
   id: number;
   title: string;
+  originalLanguage: RadarrLanguage;
   isAvailable: boolean;
   monitored: boolean;
   tmdbId: number;
@@ -32,6 +33,11 @@ export interface RadarrMovie {
   digitalRelease: string;
   inCinemas: string;
   tags: number[];
+}
+
+export interface RadarrLanguage {
+  id: number;
+  name: string | null;
 }
 
 export interface RadarrInfo {
