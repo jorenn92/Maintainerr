ed33a5e9787e3085518578d827024a2d655d724f
Ben Scobie <benscobie@users.noreply.github.com>
2024-11-28 23:10:23 +0000

    fix: Cache reset check failing in some situations
    The array was being accessed by index instead of the ID. Radarr props index & id do not align due to skipping id 1.

--- server/src/modules/rules/rules.service.ts
+++ server/src/modules/rules/rules.service.ts
@@ -1014,7 +1014,9 @@ export class RulesService {
         );
 
         //test first value
-        const first = firstValApplication.props[parsedRule.firstVal[1]];
+        const first = firstValApplication.props.find(
+          (x) => x.id == parsedRule.firstVal[1],
+        );
 
         result = first.cacheReset ? true : result;
 
@@ -1023,7 +1025,9 @@ export class RulesService {
           : undefined;
 
         // test second value
-        const second = secondValApplication?.props[parsedRule.lastVal[1]];
+        const second = secondValApplication?.props.find(
+          (x) => x.id == parsedRule.lastVal[1],
+        );
 
         result = second?.cacheReset ? true : result;
       }
