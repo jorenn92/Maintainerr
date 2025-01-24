e7843773c181d71fc6ec74ed872a26cd73ce879e
jorenn92 <jorenn92@gmail.com>
2025-01-24 12:19:20 +0100

    chore: finalize merge from main

--- server/src/modules/rules/dtos/rules.dto.ts
+++ server/src/modules/rules/dtos/rules.dto.ts
@@ -23,5 +23,4 @@ export class RulesDto {
   notifications?: Notification[];
   radarrSettingsId?: number;
   sonarrSettingsId?: number;
-  notifications?: Notification[];
 }
--- server/src/modules/rules/rules.service.ts
+++ server/src/modules/rules/rules.service.ts
@@ -30,7 +30,6 @@ import { RuleComparatorService } from './helpers/rule.comparator.service';
 import { PlexLibraryItem } from '../api/plex-api/interfaces/library.interfaces';
 import { ECollectionLogType } from '../collections/entities/collection_log.entities';
 import cacheManager from '../api/lib/cache';
-import { Notification } from '../notifications/entities/notification.entities';
 import { SonarrSettings } from '../settings/entities/sonarr_settings.entities';
 import { RadarrSettings } from '../settings/entities/radarr_settings.entities';
 import { Notification } from '../notifications/entities/notification.entities';
