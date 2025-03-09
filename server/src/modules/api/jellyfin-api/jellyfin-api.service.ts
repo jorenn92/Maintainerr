22317139d59c9a77330c5cb71c9d9b4ebac35fb4
Jolan LARGETEAU <jolanlargeteau@MacBook-Pro-de-Jolan.local>
2025-03-09 18:09:16 +0100

    fix(jellyfin): update last seen request

--- server/src/modules/api/jellyfin-api/jellyfin-api.service.ts
+++ server/src/modules/api/jellyfin-api/jellyfin-api.service.ts
@@ -54,7 +54,7 @@ export class JellyfinApiService {
         '/user_usage_stats/submit_custom_query',
         {
           CustomQueryString:
-            "SELECT DISTINCT IFNULL(NULLIF(SUBSTR(ItemName, 0, INSTR(ItemName, ' - ')), ''), ItemName) ItemName, strftime('%s', strftime('%s', DateCreated), 'unixepoch') lastView FROM PlaybackActivity WHERE SUBSTR(ItemName, 0, INSTR(ItemName, ' - ')) != '' GROUP BY IFNULL(NULLIF(SUBSTR(ItemName, 0, INSTR(ItemName, ' - ')), ''), ItemName) ORDER BY DateCreated DESC",
+            "SELECT DISTINCT IFNULL(NULLIF(SUBSTR(ItemName, 0, INSTR(ItemName, ' - ')), ''), ItemName) ItemName, max(strftime('%s', strftime('%s', DateCreated), 'unixepoch')) lastView FROM PlaybackActivity GROUP BY IFNULL(NULLIF(SUBSTR(ItemName, 0, INSTR(ItemName, ' - ')), ''), ItemName) ORDER BY DateCreated DESC",
         },
       );
       const result = response.results.find((el) => el[0] == title);
