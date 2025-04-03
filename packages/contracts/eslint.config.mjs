12488326484bb64a1b6056207734eb3c5c20e030
D <me@kumo.dev>
2025-04-03 21:09:41 +1100

    chore: fixes lint issues in server

--- packages/contracts/eslint.config.mjs
+++ packages/contracts/eslint.config.mjs
@@ -1,12 +1,17 @@
-import globals from 'globals'
 import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    languageOptions: {
+      parserOptions: { projectService: true, tsconfigRootDir: '.' },
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   eslintConfigPrettier,
