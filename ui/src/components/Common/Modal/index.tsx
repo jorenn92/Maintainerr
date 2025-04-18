b14f7e3160321ef4a375fe020569adfad5555b1a
Ben Scobie <benscobie@users.noreply.github.com>
2025-04-18 12:01:56 +0100

    fix: Modals not responsive (#1729)

--- ui/src/components/Common/Modal/index.tsx
+++ ui/src/components/Common/Modal/index.tsx
@@ -114,100 +114,96 @@ const Modal: React.FC<ModalProps> = ({
       <Transition
         appear
         as="div"
-        className="transform transition duration-300"
+        className={`relative inline-block w-full transform overflow-auto bg-zinc-700 px-4 pb-4 pt-5 text-left align-bottom shadow-xl ring-1 ring-zinc-700 transition duration-300 sm:my-8 ${maxWidthMap[size]} sm:rounded-lg sm:align-middle`}
         enterFrom="scale-75 opacity-0"
         enterTo="scale-100 opacity-100"
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
         show={!loading}
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="modal-headline"
+        style={{
+          maxHeight: 'calc(100% - env(safe-area-inset-top) * 2)',
+        }}
+        ref={modalRef}
       >
-        <div
-          className={`relative inline-block w-full overflow-auto bg-zinc-700 px-4 pb-4 pt-5 text-left align-bottom shadow-xl ring-1 ring-zinc-700 sm:my-8 ${maxWidthMap[size]} sm:rounded-lg sm:align-middle`}
-          role="dialog"
-          aria-modal="true"
-          aria-labelledby="modal-headline"
-          ref={modalRef}
-          style={{
-            maxHeight: 'calc(100% - env(safe-area-inset-top) * 2)',
-          }}
-        >
-          <div className="relative overflow-x-hidden sm:flex sm:items-center">
-            {iconSvg && <div className="modal-icon">{iconSvg}</div>}
-            <div
-              className={`mt-3 truncate text-center text-white sm:mt-0 sm:text-left ${
-                iconSvg ? 'sm:ml-4' : 'sm:mb-4'
-              }`}
-            >
-              {title && (
-                <span
-                  className="truncate text-lg font-bold leading-6"
-                  id="modal-headline"
-                >
-                  {title}
-                </span>
-              )}
-            </div>
+        <div className="relative overflow-x-hidden sm:flex sm:items-center">
+          {iconSvg && <div className="modal-icon">{iconSvg}</div>}
+          <div
+            className={`mt-3 truncate text-center text-white sm:mt-0 sm:text-left ${
+              iconSvg ? 'sm:ml-4' : 'sm:mb-4'
+            }`}
+          >
+            {title && (
+              <span
+                className="truncate text-lg font-bold leading-6"
+                id="modal-headline"
+              >
+                {title}
+              </span>
+            )}
           </div>
-          {children && (
-            <div className="relative mt-4 text-sm leading-5 text-zinc-300">
-              {children}
-            </div>
-          )}
-          {typeof onSpecial === 'function' && (
-            <div className="mt-4 flex justify-center sm:justify-end">
+        </div>
+        {children && (
+          <div className="relative mt-4 text-sm leading-5 text-zinc-300">
+            {children}
+          </div>
+        )}
+        {typeof onSpecial === 'function' && (
+          <div className="mt-4 flex justify-center sm:justify-end">
+            <Button
+              buttonType={specialButtonType}
+              onClick={onSpecial}
+              className="ml-3"
+            >
+              {specialText ? specialText : 'Special'}
+            </Button>
+          </div>
+        )}
+        {(onCancel || onOk || onSecondary || onTertiary) && (
+          <div className="relative mt-5 flex flex-row-reverse justify-center sm:mt-4 sm:justify-start">
+            {typeof onOk === 'function' && (
               <Button
-                buttonType={specialButtonType}
-                onClick={onSpecial}
+                buttonType={okButtonType}
+                onClick={onOk}
+                className="ml-3"
+                disabled={okDisabled}
+              >
+                {okText ? okText : 'Ok'}
+              </Button>
+            )}
+            {typeof onSecondary === 'function' && secondaryText && (
+              <Button
+                buttonType={secondaryButtonType}
+                onClick={onSecondary}
+                className="ml-3"
+                disabled={secondaryDisabled}
+              >
+                {secondaryText}
+              </Button>
+            )}
+            {typeof onTertiary === 'function' && tertiaryText && (
+              <Button
+                buttonType={tertiaryButtonType}
+                onClick={onTertiary}
+                className="ml-3"
+                disabled={tertiaryDisabled}
+              >
+                {tertiaryText}
+              </Button>
+            )}
+            {typeof onCancel === 'function' && (
+              <Button
+                buttonType={cancelButtonType}
+                onClick={onCancel}
                 className="ml-3"
               >
-                {specialText ? specialText : 'Special'}
+                {cancelText ? cancelText : 'Cancel'}
               </Button>
-            </div>
-          )}
-          {(onCancel || onOk || onSecondary || onTertiary) && (
-            <div className="relative mt-5 flex flex-row-reverse justify-center sm:mt-4 sm:justify-start">
-              {typeof onOk === 'function' && (
-                <Button
-                  buttonType={okButtonType}
-                  onClick={onOk}
-                  className="ml-3"
-                  disabled={okDisabled}
-                >
-                  {okText ? okText : 'Ok'}
-                </Button>
-              )}
-              {typeof onSecondary === 'function' && secondaryText && (
-                <Button
-                  buttonType={secondaryButtonType}
-                  onClick={onSecondary}
-                  className="ml-3"
-                  disabled={secondaryDisabled}
-                >
-                  {secondaryText}
-                </Button>
-              )}
-              {typeof onTertiary === 'function' && tertiaryText && (
-                <Button
-                  buttonType={tertiaryButtonType}
-                  onClick={onTertiary}
-                  className="ml-3"
-                  disabled={tertiaryDisabled}
-                >
-                  {tertiaryText}
-                </Button>
-              )}
-              {typeof onCancel === 'function' && (
-                <Button
-                  buttonType={cancelButtonType}
-                  onClick={onCancel}
-                  className="ml-3"
-                >
-                  {cancelText ? cancelText : 'Cancel'}
-                </Button>
-              )}
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+        )}
       </Transition>
     </div>,
     document.body,
